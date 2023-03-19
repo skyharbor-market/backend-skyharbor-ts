e56eec4df45c99849ab9be129461ee69b6f6a9ec
Tan <tanarin12@gmail.com>
2023-03-19 12:12:12 -0400

    add ergopay url encoding

--- src/api/ergopay.route.ts
+++ src/api/ergopay.route.ts
@@ -308,7 +308,7 @@ async function getTxDataQueryText(body: any, query: any): Promise<string | numbe
 
     const reducedTx = ReducedTransaction.from_unsigned_tx(unsignedTx, inputBoxes, inputDataBoxes, ctx)
     const txReducedBase64 = byteArrayToBase64(reducedTx.sigma_serialize_bytes())
-    //const ergoPayTx = txReducedBase64.replace(/\//g, '_').replace(/\+/g, '-')
+    const ergoPayTx = txReducedBase64.replace(/\//g, '_').replace(/\+/g, '-')
 
     // split by chunk of 1000 char to generate the QR codes
     //const ergoPayMatched = ergoPayTx.match(/.{1,1000}/g)
