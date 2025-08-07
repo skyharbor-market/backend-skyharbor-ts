7e2a0beaa21e7fde4d3e5725de6f545f526205ea
Tan <tanarin12@gmail.com>
2025-08-07 14:56:17 -0400

    fix corsfix cors

--- src/api/transactions/transactions.route.ts
+++ src/api/transactions/transactions.route.ts
@@ -1,10 +1,19 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 import { postBuyNFT } from "../../ergofunctions/transactions/buyNFT";
 import { postBulkList } from "../../ergofunctions/transactions/bulkList";
 import { postEditNFT } from "../../ergofunctions/transactions/relistNFT";
 import { postDelistNFT } from "../../ergofunctions/transactions/refund";
 
 const router = express.Router();
+
+// CORS configuration
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  methods: 'POST, OPTIONS',
+  allowedHeaders: 'Content-Type, Authorization'
+};
 
 // Handle OPTIONS preflight requests for CORS
 router.options('*', (req: Request, res: Response) => {
@@ -17,12 +26,12 @@ router.options('*', (req: Request, res: Response) => {
 // Eventually add API caching for speed, and API keys for usage
 
 // Bulk List and Single List are same method currently
-router.post(["/bulkList", "/list"], postBulkList);
+router.post(["/bulkList", "/list"], cors(corsOptions), postBulkList);
 
-router.post(["/buy"], postBuyNFT);
+router.post(["/buy"], cors(corsOptions), postBuyNFT);
 
-router.post(["/delist"], postDelistNFT);
+router.post(["/delist"], cors(corsOptions), postDelistNFT);
 
-router.post(["/edit"], postEditNFT);
+router.post(["/edit"], cors(corsOptions), postEditNFT);
 
 export default router;
