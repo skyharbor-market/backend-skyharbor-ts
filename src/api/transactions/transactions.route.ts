3ad51c15d60cbfa40460b8a43a8a67cb8cb4f808
Tan <tanarin12@gmail.com>
2025-08-07 13:54:48 -0400

    add cors for post

--- src/api/transactions/transactions.route.ts
+++ src/api/transactions/transactions.route.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { postBuyNFT } from "../../ergofunctions/transactions/buyNFT";
 import { postBulkList } from "../../ergofunctions/transactions/bulkList";
 import { postEditNFT } from "../../ergofunctions/transactions/relistNFT";
@@ -6,15 +7,43 @@ import { postDelistNFT } from "../../ergofunctions/transactions/refund";
 
 const router = express.Router();
 
+// CORS configuration to match ergopay route
+const origins = [
+  'https://skyharbor.io', 
+  'https://www.skyharbor.io', 
+  'https://v1.skyharbor.io',
+  'https://www.v1.skyharbor.io',
+  'http://localhost:3000', 
+  'http://127.0.0.1:3000', 
+  'https://testapi.skyharbor.io', 
+  'https://api.skyharbor.io', 
+  'https://skyharbor-git-development-enftexchange.vercel.app'
+];
+
+const corsOptions: cors.CorsOptions = {
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+  ],
+  methods: 'GET,OPTIONS,POST',
+  origin: origins,
+  preflightContinue: false,
+};
+
+// Handle OPTIONS preflight requests
+router.options('*', cors(corsOptions));
+
 // Eventually add API caching for speed, and API keys for usage
 
 // Bulk List and Single List are same method currently
-router.post(["/bulkList", "/list"], postBulkList);
+router.post(["/bulkList", "/list"], cors(corsOptions), postBulkList);
 
-router.post(["/buy"], postBuyNFT);
+router.post(["/buy"], cors(corsOptions), postBuyNFT);
 
-router.post(["/delist"], postDelistNFT);
+router.post(["/delist"], cors(corsOptions), postDelistNFT);
 
-router.post(["/edit"], postEditNFT);
+router.post(["/edit"], cors(corsOptions), postEditNFT);
 
 export default router;
